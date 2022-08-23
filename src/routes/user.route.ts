517a1b7433a363477967644c29c76a3f6e86e6ea
Crafter-deo <adesolaadeoluwa@gmail.com>
2022-08-23 12:05:25 +0100

    wrote layout for user routes

---
+++ src/routes/user.route.ts
@@ -0,0 +1,23 @@
+import Router from "express";
+import * as userController from "../controllers/user.controller";
+
+const router = Router();
+
+// get users
+router
+  .get("/", userController.getUsers)
+
+  // get a user
+  .get("/:id", userController.getUser)
+
+  // get user shopping cart
+  .get("/:id/cart", userController.getCart)
+
+  // get orders
+  .get("/:id/orders", userController.getOrders)
+
+  // add to shopping cart
+  .post("/:id/cart", userController.addToCart)
+  
+  // add order
+  .post("/:id/orders", userController.addOrder);
