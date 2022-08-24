70bd49e679b82f0ea416423dee5512475d11081c
Crafter-deo <adesolaadeoluwa@gmail.com>
2022-08-24 19:55:18 +0100

    added database logic

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
 import express from "express";
+import connect from "./database/index";
+import config from "./config/index";
+import adminSeeder from "./database/seeds/admin";
 import authRoutes from "./routes/auth.route";
 import userRoutes from "./routes/users.route";
 import productRoutes from "./routes/products.route";
@@ -10,7 +13,8 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 
-app.listen(3000, () => {
+app.listen(config.PORT, async () => {
   console.log("App is running");
+  await connect();
+  await adminSeeder();
 });
- 
