6d644adf742770db627f686deb7a3565d733811b
Crafter-deo <adesolaadeoluwa@gmail.com>
2022-08-24 19:58:20 +0100

    update

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,11 @@
-// set up environment variables
-import dotenv from 'dotenv';
-dotenv.config();
+require("dotenv").config();
 
 export default {
-    PORT: process.env.PORT || 3000,
-    JWT_SECRET: process.env.JWT_SECRET,
-    DB_URI: process.env.MONOGODB_URI || 'mongodb://localhost:27017/ecommerce',
-    ADMIN_USERNAME: process.env.ADMIN_USERNAME,
-    ADMIN_PASSWORD: process.env.ADMIN_PASSWORD,
-    ADMIN_EMAIL: process.env.ADMIN_EMAIL,
-}
+  PORT: process.env.PORT,
+  JWT_SECRET: process.env.JWT_SECRET,
+  MONGODB_URI: process.env.MONGODB_URI,
+  ADMIN_USERNAME: process.env.ADMIN_USERNAME,
+  ADMIN_PASSWORD: process.env.ADMIN_PASSWORD,
+  ADMIN_EMAIL: process.env.ADMIN_EMAIL,
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
+};
