2e076fef012d196c2ffd4ec1fb6df63925db252f
Crafter-deo <adesolaadeoluwa@gmail.com>
2022-08-24 21:15:38 +0100

    added adminseeder function

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 import config from "../config/index";
 import log from "../utils/logger";
+import adminSeeder from "./seeds/admin";
 
 const connect = async () => {
   try {
     await mongoose.connect(config.MONGODB_URI!);
     log.info("Database connected");
+    await adminSeeder();
   } catch (err) {
     log.error(err);
     process.exit(1);
