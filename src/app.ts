a5be25ebff2688457bef6e2f3d840b77c8c1187e
Crafter-deo <adesolaadeoluwa@gmail.com>
2022-08-24 03:13:32 +0100

    added middlewares for routes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,16 @@
-import express from 'express';
+import express from "express";
+import authRoutes from "./routes/auth.route";
+import userRoutes from "./routes/users.route";
+import productRoutes from "./routes/products.route";
 
 const app = express();
 
-app.listen(3000, ()=>{
-    console.log("App is running")
-})
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
+
+app.listen(3000, () => {
+  console.log("App is running");
+});
+ 
