1f7e4d82d7141cfc26252bd28905b8f59b42ad36
crafter-deo <adesolaadeoluwa@gmail.com>
2022-08-29 09:43:19 +0100

    added controllers for get requests

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,11 +1,23 @@
 import { Request, Response } from "express";
+import User from "../models/user.model";
+import log from "../utils/logger";
 
-export const getUsers = (req: Request, res: Response) => {
-  res.json({ message: "success" });
+export const getUsers = async (req: Request, res: Response) => {
+  try {
+    const users = await User.find();
+    return res.status(200).json({ users });
+  } catch (err) {
+    log.error(err);
+    return res.status(500).json({ error: "Internal server error" });
+  }
 };
 
-export const getUser = (req: Request, res: Response) => {
-  res.json({ message: "success" });
+export const getUser = async (req: Request, res: Response) => {
+  const foundUser = await User.findById(req.params.id);
+  if (!foundUser) {
+    return res.status(404).json({ message: "User not found" });
+  }
+  return res.status(200).json({ foundUser });
 };
 
 export const getCart = (req: Request, res: Response) => {
@@ -14,13 +26,12 @@ export const getCart = (req: Request, res: Response) => {
 
 export const getOrders = (req: Request, res: Response) => {
   res.json({ message: "success" });
-}
+};
 
-export const addToCart = (req: Request, res:Response) => {
-    res.json({message: 'success'})
-}
+export const addToCart = (req: Request, res: Response) => {
+  res.json({ message: "success" });
+};
 
-export const addOrder = (req: Request, res:Response) => {
-    res.json({message: 'success'})
-}
-
+export const addOrder = (req: Request, res: Response) => {
+  res.json({ message: "success" });
+};
